f5e9c00d735ceed3d5914e716e1d3759baa2efe8
dyakovenko <denisyakovenko057@gmail.com>
2016-04-03 22:35:37 +0300

    первый хибернет с аннотациями

---
+++ src/main/java/com/mkyong/common/Stock.java
@@ -0,0 +1,55 @@
+package com.mkyong.common;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+@Entity
+@Table(name = "stock", catalog = "mydbtest", uniqueConstraints = {
+		@UniqueConstraint(columnNames = "STOCK_NAME"),
+		@UniqueConstraint(columnNames = "STOCK_CODE") })
+public class Stock implements java.io.Serializable {
+
+	private Integer stockId;
+	private String stockCode;
+	private String stockName;
+
+	public Stock() {
+	}
+
+	public Stock(String stockCode, String stockName) {
+		this.stockCode = stockCode;
+		this.stockName = stockName;
+	}
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "STOCK_ID", unique = true, nullable = false)
+	public Integer getStockId() {
+		return this.stockId;
+	}
+
+	public void setStockId(Integer stockId) {
+		this.stockId = stockId;
+	}
+
+	@Column(name = "STOCK_CODE", unique = true, nullable = false, length = 10)
+	public String getStockCode() {
+		return this.stockCode;
+	}
+
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+
+	@Column(name = "STOCK_NAME", unique = true, nullable = false, length = 20)
+	public String getStockName() {
+		return this.stockName;
+	}
+
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	
+}
